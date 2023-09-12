391eaa1659cc0191563d95ea193e1e274d1537cc
paulina717 <129490621+paulina717@users.noreply.github.com>
2023-09-12 10:22:08 +0200

    new constructor

--- src/main/java/com/example/project/Person.java
+++ src/main/java/com/example/project/Person.java
@@ -5,6 +5,10 @@ public class Person {
     private String name;
     private int age;
 
+    public Person(String name) {
+        this.name = name;
+    }
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
